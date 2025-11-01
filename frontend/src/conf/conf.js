f1e0d515077c53012c013ec66005fb3c7fbf9292
Biswayan Paul <biswayanpaul2015@gmail.com>
2025-11-01 13:24:30 +0530

    Fix backend URL configuration to fallback to localhost if not set

--- frontend/src/conf/conf.js
+++ frontend/src/conf/conf.js
@@ -1,5 +1,6 @@
 const conf = {
-  backendUrl: String(import.meta.env.VITE_BACKEND_URL),
+  backendUrl:
+    String(import.meta.env.VITE_BACKEND_URL) || "http://localhost:8000",
 };
 
 export default conf;
